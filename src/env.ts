3031ece1a39fb2451b93a9a4483c193f1e30b8f0
Ashary Vermaysha <asharyver13@gmail.com>
2022-12-25 14:37:43 +0700

    Main ENV

---
+++ src/env.ts
@@ -0,0 +1,28 @@
+import { config } from 'dotenv'
+
+config()
+
+
+export class Env {
+  static getSpotifyClientId(): string | undefined {
+    return process.env.SPOTIFY_CLIENT_ID
+  }
+
+  static getSpotifyClienSecret(): string | undefined {
+    return process.env.SPOTIFY_CLIENT_SECRET
+  }
+
+  static getSpotifyRefreshToken(): string | undefined {
+    return process.env.SPOTIFY_REFRESH_TOKEN;
+  }
+
+  static checkEnv() {
+    if (this.getSpotifyClienSecret() === undefined) {
+      throw `The SPOTIFY_CLIENT_SECRET variable is not set`
+    }
+
+    if (this.getSpotifyClientId() === undefined) {
+      throw `The SPOTIFY_CLIENT_ID variable is not set`
+    }
+  }
+}
